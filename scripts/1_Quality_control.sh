4cf0253e4e55c6d924ad632ebe5be66f039dda1f
Dimitriadis A <athanadd@gmail.com>
2017-02-28 10:36:09 +0000

    Create 1_Quality_control.sh

---
+++ scripts/1_Quality_control.sh
@@ -0,0 +1,6 @@
+#The .fastq files were acquired.
+
+#The files were analysed using FastQC (http://www.bioinformatics.babraham.ac.uk/projects/fastqc/) without any extra options.
+
+#Trimmings were performed using trim_galore! (http://www.bioinformatics.babraham.ac.uk/projects/trim_galore/) in order to remove bad
+#reads and adapter sequences.
